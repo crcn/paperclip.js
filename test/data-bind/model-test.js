e60ff10b98c5afde25e54ee1c993d16e1a065d7c
Craig Condon <craig@spiceapps.com>
2014-04-15 12:54:58 -0700

    start on model tests

---
+++ test/data-bind/model-test.js
@@ -0,0 +1,8 @@
+var pc   = require("../.."),
+expect   = require("expect.js"),
+bindable = require("bindable"),
+nofactor = require("nofactor");
+
+describe("data-bind model#", function () {
+  
+});
