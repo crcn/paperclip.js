aaaf1435097e7346569d3a3fce62764826128a22
Craig Condon <craig@spiceapps.com>
2014-10-27 21:52:16 -0700

    add each block

--- test/block/block-each-test.js
+++ test/block/block-each-test.js
@@ -23,4 +23,12 @@ describe(__filename + "#", function () {
     view.context.set("source", [6, 7, 8, 9, 10, 11]);
     expect(view.render().toString()).to.be("67891011");
   });
+
+  it("can nest each blocks", function () {
+    var tpl = pc.template("{{#each:source}}a{{#each:model}}{{model}}{{/}}{{/}}");
+    var view = tpl.bind({ source: [[1,2], [3,4], [5,6], [7,8], [9,10]]});
+    expect(view.render().toString()).to.be("a12a34a56a78a910");
+    var view = tpl.bind({ source: [[11,12], [13,14,15]]});
+    expect(view.render().toString()).to.be("a1112a131415");
+  });
 });
