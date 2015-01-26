a3041f89495979208c135db2bb15461d90f636da
Craig Condon <craig@spiceapps.com>
2015-01-26 15:20:24 -0800

    dynamically bind attributes

--- test/components/basic-test.js
+++ test/components/basic-test.js
@@ -74,5 +74,32 @@ describe(__filename + "#", function () {
 
   it("can register a dynamic repeat component", function () {
 
+    function repeatComponent (options) {
+      var attrs = options.attributes;
+
+      function render () {
+        options.section.removeAll();
+        for (var i = 0; i < attrs.count; i++) {
+          var model = new BindableObject();
+          model.set(attrs.as, i);
+          options.section.appendChild(options.children.view(model).render());
+        }
+      }
+
+      this.bind = function (context) {
+        attrs.on("change", render);
+        render();
+      }
+    }
+
+    var tpl = template("<repeat count={{count}} as='number'>{{number}}</repeat>", { components:{repeat:repeatComponent}});
+    var v = tpl.view({count:5});
+
+    expect(v.render().toString()).to.be("01234");
+    v.context.set("count", 3);
+    expect(v.render().toString()).to.be("012");
+    v.context.set("count", 8);
+    expect(v.render().toString()).to.be("01234567");
+
   });
 });
