06991628dbdfeb5a08d4d7a5567c42655ca7a3a9
Craig Condon <craig@spiceapps.com>
2015-01-26 13:40:07 -0800

    ability to register components

--- test/components/basic-test.js
+++ test/components/basic-test.js
@@ -10,15 +10,45 @@ var tpl = paperclip.template("abba")
 
 describe(__filename + "#", function () {
 
+
+
+  function makeComponent (tpl) {
+    return function (name, attributes, children, view) {
+      var v = tpl.view();
+      this.render = function () {
+        return v.render();
+      };
+      this.bind = function (context) {
+        v.bind(context);
+      }
+      this.unbind = function () {
+
+      }
+    }
+  }
+
   it("can register a component", function () {
 
-    var tpl = template("<hello />", {
-      components: {
-        hello: function (section) {
-          console.log("OK");
-        }
-      }
-    });
+    var helloTpl = template("hello world");
 
+
+    var v = template("<hello />", {
+      components: {
+        hello: makeComponent(helloTpl)
+      }
+    }).view();
+
+    expect(v.render().toString()).to.be("hello world");
+  });
+
+  it("can register a component with a sub component", function () {
+
+    var at = template("a {{name}}"),
+    bt     = template("b <at />", { components: { at: makeComponent(at) }}),
+    ct     = template("c <bt />", { components: { bt: makeComponent(bt) }});
+
+    var v = ct.view({name:"d"});
+
+    expect(v.render().toString()).to.be("c b a d");
   });
 });
