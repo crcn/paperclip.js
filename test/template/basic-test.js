28de95c9a11b60009e6080954f5d8d529324b252
Craig Condon <craig@spiceapps.com>
2015-01-27 14:03:21 -0800

    move files

--- test/template/basic-test.js
+++ test/template/basic-test.js
@@ -70,33 +70,5 @@ describe(__filename + "#", function () {
     expect(v.render().toString()).to.be("a");
   });
 
-  it("syncs changes from the view context back to a vanilla object", function () {
-    var ctx;
-    var v = template("{{name}}").view(ctx = {name:"a"});
-    v.context.set("name", "b");
-    expect(v.render().toString()).to.be("b");
-    expect(ctx.name).to.be("b");
-  });
-
-  it("can change the context of a view", function () {
-    var v = template("{{name}}").view({name:"a"});
-    expect(v.render().toString()).to.be("a");
-    v.bind({name:"b"})
-    expect(v.render().toString()).to.be("b");
-  });
-
-  it("doesn't sync changes from previous context", function () {
-
-    var ac = new BindableObject({name:"a"}),
-    bc     = new BindableObject({name:"b"});
-
-    var v = template("{{name}}").view(ac);
-    expect(v.render().toString()).to.be("a");
-    v.bind(bc)
-    expect(v.render().toString()).to.be("b");
-    ac.set("name", "c");
-    expect(v.render().toString()).to.be("b");
-  });
-
 
 });
