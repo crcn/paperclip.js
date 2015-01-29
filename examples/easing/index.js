2cad42eb123f242668ff1c37c9d7311c5492d0d9
Craig Condon <craig@spiceapps.com>
2015-01-29 05:02:30 -0800

    start on transitions

---
+++ examples/easing/index.js
@@ -0,0 +1,29 @@
+var pc = require("../../lib/index.js"),
+stats = require("statsjs"),
+Vue = require("vue"),
+async = require("async"),
+_ = require("lodash");
+
+
+var pcTpl = 
+"<input type='text' value={{<~>count}}></input>" +
+"<div repeat.each={{ range(count) }} repeat.as='model'>" +
+    "<span easeIn={{fadeIn(node)}} easeOut={{fadeOut(node, complete)}}>{{'item ' + ~model + ' ' + ~model }}<br /></span>" +
+"</div>";
+
+var tpl = window.pcTpl = pc.template(pcTpl, pc);
+
+var view = tpl.view({
+  count: 10,
+  range: function (count) {
+    return _.range(count);
+  },
+  fadeIn: function (node) {
+    $(node).fadeIn();
+  },
+  fadeOut: function (node, complete) {
+    $(node).fadeOut(complete);
+  }
+});
+
+document.body.appendChild(view.render());
---
+++ test/attributes/easing-test.js
@@ -0,0 +1,93 @@
+var pc   = require("../.."),
+expect   = require("expect.js");
+
+describe(__filename + "#", function () {
+
+
+  xit("can add an ease in attribute", function (next) {
+
+    var v = pc.template(
+      "<div easeIn='fade'>abb</div>"
+    , {
+      attributes: {
+        easeIn: pc.Attribute.extend({
+          initialize: function () {
+            this.view.transitions.push(this);
+          },
+          enter: function () {
+            expect(this.value).to.be("fade");
+            next();
+          }
+        })
+      }
+    }).view();
+
+    v.render();
+  });
+
+  xit("can add an ease out attribute", function (next) {
+
+    var i = 0;
+
+    var v = pc.template(
+      "<div easeOut='fade' />"
+    , {
+      attributes: {
+        easeOut: pc.Attribute.extend({
+          initialize: function () {
+            this.view.transitions.push(this);
+          },
+          exit: function (complete) {
+            expect(i).to.be(1);
+            expect(this.value).to.be("fade");
+            next();
+          }
+        })
+      }
+    }).view();
+
+    v.render();
+
+    i++;
+
+    // trigger
+    v.dispose();
+  });
+
+  it("can asynchronously remove an item", function (next) {
+
+    var i = 0;
+
+    var tpl = pc.template(
+      "<div><span easeOut='fade' /></div>"
+    , {
+      attributes: {
+        easeOut: pc.Attribute.extend({
+          initialize: function () {
+            this.view.transitions.push(this);
+          },
+          exit: function (complete) {
+            var node = this.node;
+            process.nextTick(function () {
+              node.parentNode.removeChild(node);
+              complete();
+            });
+          }
+        })
+      }
+    });
+
+    var v = tpl.view();
+
+    v.render();
+    v.dispose();
+    expect(tpl._viewPool.length).to.be(0);
+    expect(v.section.node.toString()).to.be("<div><span></span></div>");
+    setTimeout(function () {
+      expect(tpl._viewPool.length).to.be(1);
+      expect(v.section.node.toString()).to.be("<div></div>");
+      next();
+    }, 10);
+  }); 
+
+});
